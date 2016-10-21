c0760c7c9220d1c02e6e8365321e9474d226a57e
Yasir Mohamed <mohamey@tcd.ie>
2016-10-21 17:37:06 +0100

    Basic PUT request script

---
+++ client/putTest.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+# Create a dummy payload to send
+payload = {
+    'sessionID' : 13318246,
+    'payload': {
+        'id': 12345678,
+        'forename': 'John',
+        'surname': 'Doe',
+        'DOB': '01/01/1970',
+        'course': 'CASE',
+        'year': 2
+        }
+    }
+
+# Convert the payload to json
+jsonPayload = json.dumps(payload)
+# Set the requests headers
+headers = {'Content-Type': 'application/json'}
+
+# Send the request, print the result
+res = requests.put('http://127.0.0.1:3000', headers = headers, data=jsonPayload)
+print(res.text)
