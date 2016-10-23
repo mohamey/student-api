ce4ed33654e3df44e76a087343d729b3dbb2f6d7
Yasir Mohamed <mohamey@tcd.ie>
2016-10-23 16:32:53 +0100

    Total refactor of JSON Responses from the api

--- main.js
+++ main.js
@@ -36,18 +36,27 @@ db.loadDatabase({}, () => {
 // Handle API Get requests
 app.get('/', (req, res) => {
   // Get the request parameters
-  const format = req.query.format.toLowerCase()
+  const responseFormat = req.query.format
   const version = parseInt(req.query.version)
   const request = req.query.get
 
+  let format
+  if (responseFormat) {
+    format = responseFormat.toLowerCase()
+  }
+
   // ensure the url parameters are valid
   if ( ((format && world.responseFormats.indexOf(format) === -1)) || !version || !request ){
-    res.send("Invalid API Request")
+    const response = {
+      successful: false,
+      message: "Malformed or Incorrect URL Parameters"
+    }
+    res.status(400).send(JSON.stringify(response))
     return
   }
 
   // Result that will be sent
-  let payload
+  let response
 
   // Switch statement to handle different API versions
   switch (version) {
@@ -58,39 +67,76 @@ app.get('/', (req, res) => {
           // Get the student ID from the url
           const studentId = req.query.id;
 
+          if (!studentId) {
+            const response = {
+              successful: false,
+              message: "No Student ID Given"
+            }
+            res.status(400).send(JSON.stringify(response))
+            return
+          }
+
           // Query the database for the id
           let studentInfo = students.findOne({id: parseInt(studentId)})
           if (studentInfo) {
             delete studentInfo['meta']
             delete studentInfo['$loki']
-            payload = studentInfo
+
+            const response = {
+              successful: true,
+              data: studentInfo,
+              message: "Student Details Retrieved"
+            }
+
+            res.status(200).send(JSON.stringify(response))
           }else{
-            payload = {}
+            const response = {
+              successful: false,
+              message: "Student doesn't exist"
+            }
+
+            res.status(404).send(JSON.stringify(response))
           }
-          break;
+          break
 
         case "all":
-          // TODO: Retrieve all students from the database send it back
           // Get all the items in the collection
           let results = students.data
 
           // If its not empty, remove lokijs metadata
-          if ( results !== [] ){
+          if ( results[0] ){
             for (let result of results){
               delete result['meta']
               delete result['$loki']
             }
-            payload = results
-          } else {
-            payload = []
-          }
-          break;
-      }
-      break;
-  }
 
-  // Send the response back
-  res.send(payload)
+            const response = {
+              successful: true,
+              data: results,
+              message: "Students retrieved"
+            }
+
+            res.status(200).send(JSON.stringify(response))
+          } else {
+            const response = {
+              successful: false,
+              message: "No Students found in Collection"
+            }
+
+            res.status(404).send(JSON.stringify(response))
+          }
+          break
+
+        default:
+          const response = {
+            successful: false,
+            message: "Unrecognized request type"
+          }
+
+          res.status(400).send(JSON.stringify(response))
+      }
+      break
+  }
 })
 
 app.post('/', (req, res) => {
@@ -98,8 +144,13 @@ app.post('/', (req, res) => {
   const payload = req.body
 
   // Ensure the necessary attributes was submitted
-  if(!req.body.id || req.body.forename || req.body.surname || req.body.DOB || req.body.course || req.body.year){
-    res.status(403).send("Incorrectly formatted content")
+  if(!req.body.id || !req.body.forename || !req.body.surname || !req.body.DOB || !req.body.course || !req.body.year){
+    const response = {
+      successful: false,
+      message: "Missing values"
+    }
+    res.status(400).send(JSON.stringify(response))
+    return
   }
 
   // Copy the desired attributes, leave out any undesired attributes
@@ -115,44 +166,73 @@ app.post('/', (req, res) => {
   // Check to make sure there's no repeat of student IDs
   const queryResult = students.findOne({id: parseInt(student.id)})
   if (!queryResult) {
-    students.insert(student)
-    res.status(200).send("Database updated")
+    const result = students.insert(student)
+    if(result){
+      const response = {
+        successful: true,
+        message: "Database updated"
+      }
+      res.status(200).send(JSON.stringify(response))
+    }else{
+      const response = {
+        successful: false,
+        message: "Error updating database"
+      }
+      res.status(501).send(JSON.stringify(response))
+    }
   }else {
-    res.status(403).send("Duplicate Student")
+    const response = {
+      successful: false,
+      message: "Duplicated Student"
+    }
+    res.status(200).send(JSON.stringify(response))
   }
 })
 
 app.put('/', (req, res) => {
-  // Ensure the necessary attributes was submitted
-  if(!req.body.id || req.body.forename || req.body.surname || req.body.DOB || req.body.course || req.body.year){
-    res.status(403).send("Incorrectly formatted content")
-  }
+  const payload = req.body
+  // Only the id attribute is necessary for this request
+  if(!payload.id){
+    const response = {
+      successful: false,
+      message: "Malformed request"
+    }
 
-  // Copy the desired attributes, leave out any undesired attributes
-  let student = {
-    id: req.body.id,
-    forename: req.body.forename,
-    surname: req.body.surname,
-    DOB: req.body.DOB,
-    course: req.body.course,
-    year: req.body.year
+    res.status(400).send(JSON.stringify(response))
+    return
   }
 
   // Retrieve the students details from the database
-  let queryResult = students.findOne({id: parseInt(student.id)})
+  let queryResult = students.findOne({id: parseInt(payload.id)})
 
   if (queryResult) {
     // Update the values of the object in the database with
     // those of the PUT request
-    for (let key in student) {
+    for (let key in payload) {
       queryResult[key] = payload[key];
     }
 
-    students.update(queryResult)
+    const result = students.update(queryResult)
 
-    res.status(200).send("Update Successful")
+    if (result) {
+      const response = {
+        successful: true,
+        message: "Update Successful"
+      }
+      res.status(200).send(JSON.stringify(response))
+    } else {
+      const response = {
+        successful: false,
+        message: "Error updating database"
+      }
+      res.status(501).send(JSON.stringify(response))
+    }
   }else{
-    res.status(404).send("Record not found")
+    const result = {
+      successful: false,
+      message: "Student record not found"
+    }
+    res.status(404).send(JSON.stringify(result))
   }
 })
 
@@ -160,18 +240,40 @@ app.delete('/', (req, res) => {
   // Get the id of the student to be deleted
   const studentID = req.body.id
 
+  // If studentID is undefined, send back an error
+  if (!studentID) {
+    const response = {
+      successful: false,
+      message: "Malformed request"
+    }
+    res.status(400).send(JSON.stringify(response))
+    return
+  }
+
   // Find the students document in the collection to be deleted by ID
   const studentDocument = students.findOne({id: parseInt(studentID)})
   if (studentDocument){
     const result = students.remove(studentDocument)
 
     if (result) {
-      res.status(200).send("Delete successful")
+      const response = {
+        successful: true,
+        message: "Delete Successful"
+      }
+      res.status(200).send(JSON.stringify(response))
     } else {
-      res.status(403).send("An error occurred deleting the document")
+      const response = {
+        successful: false,
+        message: "Internal error deleting student"
+      }
+      res.status(501).send(JSON.stringify(response))
     }
   } else{
-    res.status(404).send("Document not found")
+    const response = {
+      successful: false,
+      message: "Student not found"
+    }
+    res.status(404).send(JSON.stringify(response))
   }
 
 })
